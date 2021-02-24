35a68511a4c918769c663aefd75cd50b558768f1
StarSoftwareDevelopers <starsoftwaredev@gmail.com>
2021-02-24 10:38:13 +0800

    working dashboard

--- src/Admin/Pages/Home.js
+++ src/Admin/Pages/Home.js
@@ -1,9 +1,30 @@
+import React from 'react';
+import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Dashboard from './../AdminComponents/Dashboard';
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
+
 const Home = () => {
-    return (
-        <div>
-            <h1>Admin Home</h1>
-        </div>
-      );
+    return ( 
+      <div>
+          <Container maxWidth = "lg" style={{
+              margin: "1rem"
+          }}>
+            <ThemeProvider theme={theme}>
+                <Typography variant="h4" style={{
+                    textAlign: "left",
+                    marginTop: "-20px",
+                    marginBottom: "1rem"
+                    }}>Overview</Typography>
+                    <Dashboard/>
+            </ThemeProvider>
+      </Container>
+      </div>
+      
+     );
 }
  
 export default Home;
