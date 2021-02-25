2d43c8d10ec0366701fe7b8f6673c38b0b118cef
StarSoftwareDevelopers <starsoftwaredev@gmail.com>
2021-02-25 12:33:29 +0800

    google sign in worked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } 
+import React,{Component} from 'react';
+import { BrowserRouter, Route, Switch, Redirect } 
   from 'react-router-dom'; 
 
 import MainLayout from  './layout/mainLayout';
@@ -14,35 +14,79 @@ import Home from './Admin/Pages/Home';
 
 import './default.scss';
 
-function App() {
-  return (
-    <div className="App">
-      
-      
-      <Switch>
-          <Route exact path='/' render={() => (
-            <HomeLayout>
-              <Homepage />
-            </HomeLayout>
-          )}/> 
-          <Route exact path='/registration' render={() => (
-            <MainLayout>
-              <Registration />
-            </MainLayout>
-          )}/> 
-           <Route exact path='/login' render={() => (
-            <MainLayout>
-              <Login />
-            </MainLayout>
-          )}/> 
-          <AppBar/>
-          <Route exact path="admin" component={Home}/>
-         
-          
-      </Switch>
-      
-      </div>
-  );
+import { auth, handleUserProfile } from './firebase/utils';
+
+const initialState = {
+  currentUser : null
+};
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ...initialState
+    };
+  }
+
+  //eventListener
+  authListener = null;
+
+  componentDidMount(){
+    	this.authListener = auth.onAuthStateChanged(async userAuth => {
+        if (userAuth){
+          const userRef = await handleUserProfile(userAuth);
+          userRef.onSnapshot(snapshot => {
+            this.setState({
+              currentUser: {
+                id: snapshot.id,
+                ...snapshot.data()
+              }
+            })
+          })
+        }
+
+        this.setState({
+          ...initialState
+        });
+      });
+  }
+
+  componentWillUnmount(){
+      this.authListener();
+  }
+
+  render() {
+    const { currentUser } = this.state;
+
+    return (
+      <div className="App"> 
+        <Switch>
+            <Route exact path='/' render={() => (
+              <HomeLayout currentUser={currentUser}>
+                <Homepage />
+              </HomeLayout>
+            )}/> 
+            <Route exact path='/registration' render={() => (
+              <MainLayout currentUser={currentUser}>
+                <Registration />
+              </MainLayout>
+            )}/> 
+             <Route exact path='/login' 
+              render={() => currentUser ? <Redirect to="/"/> :  (
+                <MainLayout currentUser={currentUser}>
+                  <Login />
+                </MainLayout>
+              )}/> 
+            <AppBar/>
+            <Route exact path="admin" component={Home}/>
+           
+            
+        </Switch>
+        
+        </div>
+    );
+  }
+ 
 }
 
 export default App;
