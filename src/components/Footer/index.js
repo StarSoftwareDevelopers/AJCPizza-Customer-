b682394678bb71b7685c9a31ce800ee2a4b78b42
StarSoftwareDevelopers <starsoftwaredev@gmail.com>
2021-02-28 21:32:57 +0800

    pushed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import Logo from './../../assets/AJC Pizza Logo.png';
 import './style.scss';
+import {
+    FaFacebook,
+    FaInstagram,
+    FaTwitter
+} from 'react-icons/fa';
+
+import {
+    FooterContainer,
+    FooterWrap,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    SocialIcons,
+    SocialIconsLink
+} from './FooterElem';
+
 
 const Footer = props => {
     return (
-        <footer className="footr">
-            <div className="wrap">
-                AJC Pizza
-            </div>
-        </footer>
+       <FooterContainer>
+           <FooterWrap>
+               <SocialMedia>
+                   <SocialMediaWrap>
+                       <SocialLogo>
+                       <img src={Logo} alt="AJC Logo" />
+                       </SocialLogo>
+                       <SocialIconsLink href="/" target="_blank" aria-label="Facebook" rel="moopener noreferrer">
+                           <FaFacebook />
+                       </SocialIconsLink>
+                       <SocialIconsLink href="/" target="_blank" aria-label="Instagram" rel="moopener noreferrer">
+                           <FaFacebook />
+                       </SocialIconsLink>
+                       <SocialIconsLink href="/" target="_blank" aria-label="Twitter" rel="moopener noreferrer">
+                           <FaFacebook />
+                       </SocialIconsLink>
+                   </SocialMediaWrap>
+               </SocialMedia>
+           </FooterWrap>
+       </FooterContainer>
+                 
+                    
     );
 }
 
