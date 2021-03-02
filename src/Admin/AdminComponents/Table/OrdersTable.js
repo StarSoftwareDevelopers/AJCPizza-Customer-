6c149407d1febc6af97539c101bcc7c0d9d38332
StarSoftwareDevelopers <starsoftwaredev@gmail.com>
2021-03-02 11:03:04 +0800

    dummy data and dialog

--- src/Admin/AdminComponents/Table/OrdersTable.js
+++ src/Admin/AdminComponents/Table/OrdersTable.js
@@ -2,22 +2,31 @@ import React from 'react';
 import MUIDataTable from "mui-datatables";
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-
+import MenuItem from "@material-ui/core/MenuItem";
 import {Select,FormControl} from '@material-ui/core';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 
 const OrderTable = () => {
 
     const [value, setValue] = React.useState("");
+    const [status, setStatus] = React.useState(false);
+
     const [open, setOpen] = React.useState(false);
 
-    const handleOpen = () => {
+    const handleClickOpen = () => {
       setOpen(true);
     };
   
     const handleClose = () => {
       setOpen(false);
     };
+
 
     const [state, setState] = React.useState({
         age: '',
@@ -31,13 +40,10 @@ const OrderTable = () => {
           [name]: event.target.value,
         });
       };
-
-      //for the select
       
-      const handleChangeSelect = (event) => {
-        setValue(event.target.value)
-        console.log(value)
-      }
+    // const handleClick = () => {
+    //   window.confirm("Are you sure?")
+    // };    
     
     const columns = ["No.", "Order Date", "Delivery Date", "Order ID","Customer Name","Order Amount","Payment Status","Payment Amount","View Details","Order Status"];
 
@@ -47,19 +53,18 @@ const OrderTable = () => {
         View
     </Button>,
      <FormControl>
-      <Select native
+      <Select 
           labelId="select-demo"
           id="status-select"
-          value={value}
-          displayEmpty
-          onChange={handleChangeSelect}
+          value={status}
+          onChange={handleChange}
         > 
-          <option aria-label="None" value="" disabled/>
-          <option value={1}>Confirmed</option>
-          <option value={2}>Preparing</option>
-          <option value={3}>On the Way</option>
-          <option value={4}>On the Way (Delayed)</option>
-          <option value={5}>Completed</option>
+          {/* <option aria-label="None" value="" disabled/> */}
+         <MenuItem value ={10} onClick={handleClickOpen}>Confirmed</MenuItem>
+         <MenuItem value ={20}>Preparing</MenuItem>
+         <MenuItem value ={30}>On the Way</MenuItem>
+         <MenuItem value ={40}>On the Way (Delayed)</MenuItem>
+         <MenuItem value ={50}>Delivered</MenuItem>
       </Select>
     </FormControl>
   ], 
@@ -67,8 +72,6 @@ const OrderTable = () => {
     <Button variant="outlined" color="primary">
         View
     </Button>,
-
-
 ],];
 
     const options = {
@@ -86,6 +89,28 @@ const OrderTable = () => {
                 />
 
             </Container>
+
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">{"Order Status"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  Set the order status to "Confirmed"?
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                  No
+                </Button>
+                <Button onClick={handleClose} color="primary">
+                  Yes
+                </Button>
+              </DialogActions>
+      </Dialog>
         </div>
     );
 }
